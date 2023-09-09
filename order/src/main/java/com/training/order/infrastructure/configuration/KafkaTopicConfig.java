dc30b61177957db6490b76f500e258aa8c26d3f6
erik.gonzalez <erik.gonzalez@ozona.es>
2023-09-10 01:09:27 +0200

    Changed git repo url for the new config one Added Dockerfile for config micro

--- order/src/main/java/com/training/order/infrastructure/configuration/KafkaTopicConfig.java
+++ order/src/main/java/com/training/order/infrastructure/configuration/KafkaTopicConfig.java
@@ -2,6 +2,7 @@ package com.training.order.infrastructure.configuration;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaAdmin;
@@ -12,13 +13,13 @@ import java.util.Map;
 @Configuration
 public class KafkaTopicConfig {
 
-//    @Value(value = "${spring.kafka.bootstrap-servers}")
-//    private String bootstrapAddress;
+    @Value("${kafka-bootstrap-address}")
+    private String kafkaBootstrapAddress;
 
     @Bean
     public KafkaAdmin kafkaAdmin() {
         Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapAddress);
         return new KafkaAdmin(configs);
     }
 
