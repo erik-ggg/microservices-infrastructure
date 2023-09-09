53f98944fff2e7985eb7d79da44089cd91f79f0d
erik.gonzalez <erik.gonzalez@ozona.es>
2023-09-09 23:09:25 +0200

    Enabled config server and added the properties data inside both config server and order micro

--- order/src/main/java/com/training/order/infrastructure/configuration/KafkaProducerConfig.java
+++ order/src/main/java/com/training/order/infrastructure/configuration/KafkaProducerConfig.java
@@ -2,6 +2,7 @@ package com.training.order.infrastructure.configuration;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -14,12 +15,15 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
+    @Value("${kafka-bootstrap-address}")
+    private String kafkaBootstrapAddress;
+
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(
                 ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:29092");
+                kafkaBootstrapAddress);
         configProps.put(
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                 StringSerializer.class);
